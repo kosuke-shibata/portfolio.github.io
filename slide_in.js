fd757efc8ca97a893b02d42bbc5e158d231a563d
kosuke-shibata <83070201+kosuke-shibata@users.noreply.github.com>
2021-11-27 14:08:14 +0900

    Create slide_in.js

---
+++ slide_in.js
@@ -0,0 +1,7 @@
+$('.menu-button').on('click', function() {
+  if ($('.menu-command').hasClass('menu-open')) {
+    $('.menu-command').removeClass('menu-open');
+  } else {
+    $('.menu-command').addClass('menu-open');
+  }
+});
