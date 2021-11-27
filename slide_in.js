5c72cf58370fae40a83cc9bcd3ccce16d03759b5
kosuke-shibata <83070201+kosuke-shibata@users.noreply.github.com>
2021-11-27 14:05:44 +0900

    Delete slide_in.js

--- slide_in.js
+++
@@ -1,7 +0,0 @@
-$('.menu-button').on('click', function(){
-  if ($('.menu-command').hasClass('menu-open')) {
-    $('.menu-command').removeClass('menu-open');
-  } else {
-    $('.menu-command').addClass('menu-open');
-  }
-});
