d139b9cd95e0033efb64601ea949a3d45b6b6b29
kosuke-shibata <83070201+kosuke-shibata@users.noreply.github.com>
2022-09-23 22:16:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
+
 $(function () {
-  let About = (($('#about').offset().top) - 500);
+  let About = (($('#about').offset().top) - 600);
+  let Service = (($('#services').offset().top) - 600);
+  let Works = (($('#works').offset().top) - 600);
   $(window).scroll(function () {
     if ($(this).scrollTop() >= About) {
       $('#about').addClass('about-in');
     }
-  });
-});
-
-
-$(function () {
-  let Works = (($('#works').offset().top) - 500);
-  let Contact = (($('#contact').offset().top) - 500);
-  $(window).scroll(function () {
+    if ($(this).scrollTop() >= Service) {
+      $('.services-title').addClass('slide-in');
+    }
     if ($(this).scrollTop() >= Works) {
       $('.works-title').addClass('slide-in');
     }
-    if ($(this).scrollTop() >= Contact) {
-      $('.contact-title').addClass('slide-in');
-    }
   });
 });
+
+$(window).on('touchmove.noScroll', function(e) {
+  e.preventDefault();
+});
+
+$(window).off('.noScroll');
